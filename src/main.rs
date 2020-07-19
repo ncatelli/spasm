dd399b8f179ce578c2f39695c9592b0163dd87ea
Nate Catelli <ncatelli@packetfire.org>
2020-07-19 12:23:43 -0500

    Stub out help and assemble commands

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,25 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::process;
 
+const HELP_STRING: &'static str = "A command-line 6502 assembler
+Usage: spasm <command> [*.asm]
+Commands:
+  assemble: assemble a source 6502 assembly file to an object file.
+  help:     print this help message
+";
+
 enum RuntimeError {
     InvalidArguments,
-    Other(String),
+    FileUnreadable,
+    Undefined(String),
 }
 
 impl fmt::Debug for RuntimeError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::InvalidArguments => write!(f, "invalid number of arguments."),
-            Self::Other(s) => write!(f, "{}", s),
+            Self::InvalidArguments => write!(f, "invalid number of arguments"),
+            Self::FileUnreadable => write!(f, "source file unreadable"),
+            Self::Undefined(s) => write!(f, "{}", s),
         }
     }
 }
@@ -22,30 +31,33 @@ type RuntimeResult<T> = Result<T, RuntimeError>;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let args_len = args.len();
-
-    match args_len {
-        2 => run_file(&args[1]).expect("Unable to parse file"),
-        _ => {
-            println!("Usage: spasm [*.asm]");
-            process::exit(64);
-        }
+    if args.len() != 3 {
+        help().unwrap();
+        process::exit(1);
     }
+
+    match (&args[1].as_ref(), &args[2]) {
+        (&"assemble", filename) => run(read_src_file(filename).unwrap()),
+        _ => process::exit(help().unwrap()),
+    }
+    .unwrap();
 }
 
-fn run_file(filename: &str) -> Result<(), RuntimeError> {
-    let mut f = File::open(filename).expect("file not found");
+fn read_src_file(filename: &str) -> RuntimeResult<String> {
+    let mut f = File::open(filename).map_err(|_| RuntimeError::FileUnreadable)?;
 
     let mut contents = String::new();
     match f.read_to_string(&mut contents) {
-        Ok(_) => {
-            run(contents).unwrap();
-            Ok(())
-        }
-        Err(e) => Err(RuntimeError::Other(e.to_string())),
+        Ok(_) => Ok(contents),
+        Err(e) => Err(RuntimeError::Undefined(e.to_string())),
     }
 }
 
-fn run(_source: String) -> RuntimeResult<()> {
-    Ok(())
+fn help() -> RuntimeResult<i32> {
+    println!("{}", HELP_STRING);
+    Ok(0)
+}
+
+fn run(_source: String) -> RuntimeResult<i32> {
+    Ok(0)
 }
