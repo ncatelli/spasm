79347a08b0d8991c4134c2b5d8909a7ed7a9732e
Nate Catelli <ncatelli@packetfire.org>
2020-08-16 11:53:17 -0500

    Add addressing modes

--- src/asm/instruction.rs
+++ src/asm/instruction.rs
@@ -76,7 +76,55 @@ pub enum Mnemonic {
     NOP,
 }
 
+// AddressMode represents the 6502 addressing mode only.
 pub enum AddressMode {
+    Accumlator,
+    Implied,
+    Immediate,
+    Absolute,
+    ZeroPage,
+    Relative,
+    AbsoluteIndirect,
+    AbsoluteIndexedWithX,
+    AbsoluteIndexedWithY,
+    ZeroPageIndexedWithX,
+    ZeroPageIndexedWithY,
+    ZeroPageIndexedIndirect,
+    ZeroPageIndirectIndexedWithY,
+}
+
+impl From<AddressModeWithOperand> for AddressMode {
+    fn from(am: AddressModeWithOperand) -> AddressMode {
+        let ref_am = am;
+        ref_am.into()
+    }
+}
+
+impl From<&AddressModeWithOperand> for AddressMode {
+    fn from(am: &AddressModeWithOperand) -> AddressMode {
+        match am {
+            AddressModeWithOperand::Accumlator => AddressMode::Accumlator,
+            AddressModeWithOperand::Implied => AddressMode::Implied,
+            AddressModeWithOperand::Immediate(_) => AddressMode::Immediate,
+            AddressModeWithOperand::Absolute(_) => AddressMode::Absolute,
+            AddressModeWithOperand::ZeroPage(_) => AddressMode::ZeroPage,
+            AddressModeWithOperand::Relative(_) => AddressMode::Relative,
+            AddressModeWithOperand::AbsoluteIndirect(_) => AddressMode::AbsoluteIndirect,
+            AddressModeWithOperand::AbsoluteIndexedWithX(_) => AddressMode::AbsoluteIndexedWithX,
+            AddressModeWithOperand::AbsoluteIndexedWithY(_) => AddressMode::AbsoluteIndexedWithY,
+            AddressModeWithOperand::ZeroPageIndexedWithX(_) => AddressMode::ZeroPageIndexedWithX,
+            AddressModeWithOperand::ZeroPageIndexedWithY(_) => AddressMode::ZeroPageIndexedWithY,
+            AddressModeWithOperand::ZeroPageIndexedIndirect(_) => {
+                AddressMode::ZeroPageIndexedIndirect
+            }
+            AddressModeWithOperand::ZeroPageIndirectIndexedWithY(_) => {
+                AddressMode::ZeroPageIndirectIndexedWithY
+            }
+        }
+    }
+}
+
+pub enum AddressModeWithOperand {
     Accumlator,
     Implied,
     Immediate(u8),
