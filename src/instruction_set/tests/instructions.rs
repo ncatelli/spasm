666ea39ccea06251c636b6465f377275956db006
Nate Catelli <ncatelli@packetfire.org>
2020-08-18 19:48:22 -0500

    Add OpCode matrix (#11)
    * Stub opcodes module
    
    * WIP: stubbing out type translations
    
    * WIP opcode -> mnemonic
    
    * Mapping of octals to mnemonics.
    
    * OpCode -> OpCodeOctal
    
    * Update address modes
    
    * Add instruction set conversions.
    
    * Remove unused references.
    
    * Check in lock file
    
    * remove unnecessary opcode module.
    
    * convert address to bytes
    
    * Covert instructions to a vec.
    
    * Consolidate AddressMode types.
    
    * Convert instructions to bytes.

---
+++ src/instruction_set/tests/instructions.rs
@@ -0,0 +1,27 @@
+use crate::instruction_set::address_mode::AddressMode;
+use crate::instruction_set::mnemonics::Mnemonic;
+use crate::instruction_set::Instruction;
+
+#[test]
+fn instruction_with_accumulator_address_mode_should_return_single_byte() {
+    let inst = Instruction::new(Mnemonic::ASL, AddressMode::Accumulator);
+    let op: Vec<u8> = inst.into();
+
+    assert_eq!(op, vec![0x0a])
+}
+
+#[test]
+fn instruction_with_single_byte_operand_should_order_instructions_correctly() {
+    let inst = Instruction::new(Mnemonic::CPY, AddressMode::Immediate(0x12));
+    let op: Vec<u8> = inst.into();
+
+    assert_eq!(op, vec![0xc0, 0x12])
+}
+
+#[test]
+fn instruction_with_two_byte_operand_should_order_operands_after_opcode_in_little_endian_format() {
+    let inst = Instruction::new(Mnemonic::CPY, AddressMode::Absolute(0x1234));
+    let op: Vec<u8> = inst.into();
+
+    assert_eq!(op, vec![0xcc, 0x34, 0x12])
+}
--- src/instruction_set/tests/mod.rs
+++ src/instruction_set/tests/mod.rs
@@ -1,16 +1,2 @@
-use crate::instruction_set::address_mode::{AddressMode, AddressModeWithOperand};
-
-#[test]
-fn address_mode_with_operand_should_cast_into_corresponding_address_mode_type() {
-    let amwo: AddressModeWithOperand = AddressModeWithOperand::Accumlator;
-    let am: AddressMode = amwo.into();
-
-    assert!(am == AddressMode::Accumlator);
-}
-
-#[test]
-fn address_mode_with_operand_should_be_comparable_to_address_mode() {
-    let amwo: AddressModeWithOperand = AddressModeWithOperand::Accumlator;
-
-    assert!(amwo == AddressMode::Accumlator);
-}
+mod address_mode;
+mod instructions;
