b9303f1bca71e89127d721364ef8be68796f61b1
Nate Catelli <ncatelli@packetfire.org>
2020-08-21 22:45:35 -0500

    Additional test case.

--- src/parser/tests/mod.rs
+++ src/parser/tests/mod.rs
@@ -15,3 +15,16 @@ fn should_parse_valid_nop_instruction() {
         instruction().parse(&input)
     );
 }
+
+#[test]
+fn should_strip_arbitrary_length_leading_chars_from_instruction() {
+    let input: Vec<char> = "    nop\n".chars().collect();
+
+    assert_eq!(
+        Ok(MatchStatus::Match((
+            &input[8..],
+            Instruction::new(Mnemonic::NOP, AddressMode::Implied)
+        ))),
+        instruction().parse(&input)
+    );
+}
