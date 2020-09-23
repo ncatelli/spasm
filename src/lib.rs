8e5fa1d83290a91462e8e52fd859ace6b8584aac
Nate Catelli <ncatelli@packetfire.org>
2020-09-23 02:18:29 +0000

    symbol config type

--- src/lib.rs
+++ src/lib.rs
@@ -11,18 +11,22 @@ mod addressing;
 use addressing::SizeOf;
 mod parser;
 
+/// A type storing the results of an assemble representing an array of bytes
+/// or a String Error.
 pub type AssemblerResult = Result<Vec<u8>, String>;
+
+type SymbolConfig = HashMap<String, usize>;
 
 // Converts a source string to it's corresponding array of little endinan binary
 // opcodes.
 pub fn assemble(source: &str) -> AssemblerResult {
-    let opcodes: Vec<u8> = match parser::instructions().parse(&source).unwrap() {
+    let (_, _symbols, insts) = match parser::instructions().parse(&source).unwrap() {
         parcel::MatchStatus::Match((_, insts)) => Ok(insts),
         _ => Err("match error".to_string()),
     }?
     .into_iter()
     .fold(
-        (0 as usize, HashMap::<String, usize>::new(), Vec::new()),
+        (0 as usize, SymbolConfig::new(), Vec::new()),
         |(offset, mut labels, mut insts), ios| match ios {
             InstructionOrSymbol::Instruction(i) => {
                 insts.push(addressing::Positional::with_position(offset, i));
@@ -33,13 +37,14 @@ pub fn assemble(source: &str) -> AssemblerResult {
                 (offset, labels, insts)
             }
         },
-    ) // DEVNOTE: temp unpack of instructions from InstructionOrSymbol enum
-    .2
-    .into_iter()
-    .map(|pi| pi.unwrap())
-    .map(Into::<Vec<u8>>::into)
-    .flatten()
-    .collect::<Vec<u8>>();
+    );
+
+    let opcodes = insts
+        .into_iter()
+        .map(|pi| pi.unwrap())
+        .map(Into::<Vec<u8>>::into)
+        .flatten()
+        .collect::<Vec<u8>>();
 
     Ok(opcodes)
 }
