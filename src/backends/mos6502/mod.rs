ddf706968f783c3beed330d7108290a3f1bdebb4
Nate Catelli <ncatelli@packetfire.org>
2020-10-21 23:26:22 +0000

    Add empty preparser assembler

--- src/backends/mos6502/mod.rs
+++ src/backends/mos6502/mod.rs
@@ -13,6 +13,7 @@ use crate::backends::mos6502::instruction_set::address_mode::{
 use crate::backends::mos6502::instruction_set::{
     InstructionOrDefinition, Mnemonic, StaticInstruction,
 };
+use crate::preparser;
 use crate::Emitter;
 use crate::{Assembler, AssemblerResult};
 
@@ -89,5 +90,71 @@ impl Assembler<&str> for MOS6502Assembler {
             .collect::<Vec<u8>>();
 
         Ok(opcodes)
+    }
+}
+
+impl Assembler<preparser::Token> for MOS6502Assembler {
+    fn assemble(&self, source: preparser::Token) -> AssemblerResult {
+        /*        let src = source.chars().collect::<Vec<char>>();
+        let (_, labels, symbols, insts) = match parser::instructions().parse(&src).unwrap() {
+            parcel::MatchStatus::Match((_, insts)) => Ok(insts),
+            _ => Err("match error".to_string()),
+        }?
+        .into_iter()
+        .enumerate()
+        .fold(
+            (0, LabelMap::new(), SymbolMap::new(), Vec::new()),
+            |(offset, mut labels, mut symbols, mut insts), (line, iod)| match iod {
+                InstructionOrDefinition::Instruction(i) => {
+                    let size_of = i.size_of();
+                    let line_number = line + 1;
+                    insts.push((
+                        line_number,
+                        addressing::Positional::with_position(offset, i),
+                    ));
+                    (offset + size_of, labels, symbols, insts)
+                }
+                InstructionOrDefinition::Label(l) => {
+                    labels.insert(l, offset as u16);
+                    (offset, labels, symbols, insts)
+                }
+                InstructionOrDefinition::Symbol((s, v)) => {
+                    symbols.insert(s, v);
+                    (offset, labels, symbols, insts)
+                }
+            },
+        );
+
+        let opcodes = insts
+            .into_iter()
+            .map(|(line, pi)| (line, pi.unwrap()))
+            .map(|(line, i)| {
+                let mnemonic = i.mnemonic;
+                let amor = i.amor;
+                match amor {
+                    AddressModeOrReference::Label(l) => labels.get(&l).map_or(
+                        Err(format!("label {}, undefined at line: {}", &l, line)),
+                        |offset| Ok((mnemonic, AddressMode::Absolute(*offset))),
+                    ),
+                    AddressModeOrReference::Symbol(s) => symbols.get(&s.symbol).map_or(
+                        Err(format!("symbol {}, undefined at line: {}", &s.symbol, line)),
+                        |byte_value| Ok((mnemonic, AddressMode::Immediate(*byte_value))),
+                    ),
+                    AddressModeOrReference::AddressMode(am) => Ok((mnemonic, am)),
+                }
+            })
+            .collect::<Result<Vec<(Mnemonic, AddressMode)>, String>>()?
+            .into_iter()
+            .map(|ti| StaticInstruction::new(ti.0, ti.1))
+            .map(|si| {
+                let mc: Vec<u8> = si.emit();
+                mc
+            })
+            .flatten()
+            .collect::<Vec<u8>>();
+
+        Ok(opcodes)
+        */
+        Err("Unimplemented".to_string())
     }
 }
